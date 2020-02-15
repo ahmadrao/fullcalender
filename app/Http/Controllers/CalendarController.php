96d1324bed58fe0d5bfbc86294d97a84d5006fe6
Muhammad Ahmad <ahmadraosanawarali@gmail.com>
2020-02-15 23:49:22 +0500

    FullCalendar App is working fine

---
+++ app/Http/Controllers/CalendarController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Calendar;
+use Illuminate\Http\Request;
+use App\Http\Resources\CalenderResource;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
+class CalendarController extends Controller
+{
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        return CalenderResource::collection(Calendar::all());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $new_calendar = Calendar::create($request->all());
+        return response()->json([
+            'data' => new CalenderResource($new_calendar),
+            'message' => 'Successfully added new event!',
+            'status' => Response::HTTP_CREATED
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Calendar  $calendar
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Calendar $calendar)
+    {
+        return response($calendar, Response::HTTP_OK);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Calendar  $calendar
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Calendar $calendar)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Calendar  $calendar
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Calendar $calendar)
+    {
+        $calendar->update($request->all());
+        return response()->json([
+            'data' => new CalenderResource($calendar),
+            'message' => 'Successfully updated event!',
+            'status' => Response::HTTP_ACCEPTED
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Calendar  $calendar
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Calendar $calendar)
+    {
+        $calendar->delete();
+        return response('Event removed successfully!', Response::HTTP_NO_CONTENT);
+    }
+}
